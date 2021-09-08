1149049d6833107a2192412b83eb0e57395bd135
Marcus <aichmayr.marcus@gmail.com>
2021-09-08 11:25:58 +0200

    change input of returned function from 'f_exp' and 'f_pol', add 'f_exp_pol' to reduce code duplication

--- bijectivity_exponential_maps/functions.py
+++ bijectivity_exponential_maps/functions.py
@@ -14,9 +14,9 @@ from sage.functions.log import exp
 from sage.modules.free_module_element import vector
 from sage.matrix.special import ones_matrix
 
-def f_pol(W, Wt, c=None):
+def f_exp_pol(W, Wt, c=None, mode="exp"):
     r"""
-    Returns the polynomial map determined by the matrices ``W`` and ``Wt``.
+    Returns the exponential or polynomial map determined by the matrices ``W`` and ``Wt``.
     
     INPUT:
     
@@ -25,6 +25,8 @@ def f_pol(W, Wt, c=None):
     - ``Wt`` -- a matrix
     
     - ``c`` -- a vector (optional)
+    
+    - ``mode`` -- a string. Either 'exp' (default) or 'pol'.
     
     OUTPUT:
     
@@ -40,10 +42,15 @@ def f_pol(W, Wt, c=None):
     
     (d, n) = W.dimensions()
     
-    def f(x):
-        return vector([sum([W[i,j] * c[j] * prod([x[k]**Wt[k,j] for k in range(d)]) for j in range(n)]) for i in range(d)])
+    if mode == "exp":
+        def f(*x):
+            return sum([c[i] * exp(Wt.column(i).dot_product(vector(x))) * W.column(i) for i in range(n)])
+    elif mode == "pol":
+        def f(*x):
+            return vector([sum([W[i,j] * c[j] * prod([x[k]**Wt[k,j] for k in range(d)]) for j in range(n)]) for i in range(d)])
+    else:
+        raise ValueError("Mode '" + mode + "' is not available. Try 'exp' or 'pol'.")
     return f
-
 
 def f_exp(W, Wt, c=None):
     r"""
@@ -61,16 +68,22 @@ def f_exp(W, Wt, c=None):
     
     - If ``c`` is omitted, the result take the vector consisting of ones.
     """
-    if W.dimensions() != Wt.dimensions():
-        raise ValueError('Matrices must have same dimensions.')
+    return f_exp_pol(W, Wt, c, mode="exp")
 
-    if c == None:
-        c = vector(ones_matrix(1, W.ncols()))
-    elif len(c) != W.ncols():
-        raise ValueError('Number of columns and dimension of ``c`` do not match.')
+def f_pol(W, Wt, c=None):
+    r"""
+    Returns the polynomial map determined by the matrices ``W`` and ``Wt``.
     
-    (d, n) = W.dimensions()
+    INPUT:
     
-    def f(x):
-        return sum([c[i] * exp(Wt.column(i).dot_product(vector(x))) * W.column(i) for i in range(n)])
-    return f
+    - ``W`` -- a matrix
+    
+    - ``Wt`` -- a matrix
+    
+    - ``c`` -- a vector (optional)
+    
+    OUTPUT:
+    
+    - If ``c`` is omitted, the result take the vector consisting of ones.
+    """
+    return f_exp_pol(W, Wt, c, mode="pol")
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -11,10 +11,10 @@ class Tests(unittest.TestCase):
         Wt
         c = vector([1,2,4])
         fc = f_pol(W, Wt, c)
-        self.assertEqual(fc([1,2]), vector([-3, 0]))
+        self.assertEqual(fc(1,2), vector([-3, 0]))
 
         fc = f_pol(W, Wt) # without c
-        self.assertEqual(fc([1,2]), vector([0, 1]))
+        self.assertEqual(fc(1,2), vector([0, 1]))
 
     # Todo: Tests for f_exp
     def test_f_pol(self):
@@ -24,10 +24,10 @@ class Tests(unittest.TestCase):
         Wt
         c = vector([1,2,4])
         Fc = f_exp(W, Wt, c)
-        Fc([1,2])
+        Fc(1,2)
 
         Fc = f_exp(W, Wt) # without c
-        Fc([1,2])
+        Fc(1,2)
 
 if __name__ == '__main__':
     unittest.main()
