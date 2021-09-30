31037ff9920b898bef829c6d0bd82b8570d4d7cc
Marcus <aichmayr.marcus@gmail.com>
2021-09-30 16:55:54 +0200

    add explanations

--- bijectivity_exponential_maps/conditions_bijectivity.py
+++ bijectivity_exponential_maps/conditions_bijectivity.py
@@ -4,6 +4,9 @@ In this module, we check bijectivity for exponential maps.
 EXAMPLES::
 
     sage: from bijectivity_exponential_maps import *
+
+Let us consider the following matrices::
+
     sage: W = matrix([[1,0,1,0],[0,1,0,1]])
     sage: W
     [1 0 1 0]
@@ -12,31 +15,78 @@ EXAMPLES::
     sage: Wt
     [ 1  0  0 -1]
     [ 0  1  1  1]
+
+    sage: var('x1, x2, c1, c2, c3, c4')
+    (x1, x2, c1, c2, c3,c 4)
+    sage: c = [c1, c2, c3, c4]
+
+Therefore, we obtain the following exponential map::
+
+    sage: Fc = f_exp(W, Wt, c)
+    sage: Fc(x1, x2)
+    (c1*e^x1 + c3*e^x2, c4*e^(-x1 + x2) + c2*e^x2)
+
+Hence, we obtain the oriented matroids::
+
     sage: from sign_vectors.oriented_matroids import *
     sage: covectors_from_matrix(W, kernel=True, algorithm='fe', separate=True)
     [[(0000)], [(0+0-), (-0+0), (+0-0), (0-0+)], [(-++-), (++--), (+--+), (--++)]]
     sage: covectors_from_matrix(Wt, algorithm='fe', separate=True)
     [[(0000)], [(+00-), (+++0), (-00+), (---0), (0---), (0+++)], [(+++-), (---+), (----), (+---), (++++), (-+++)]]
+
+We can check injectivity by using the function :func:`~cond_inj_intersection`::
+
     sage: cond_inj_intersection(W, Wt)
     True
+
+Therefore, the corresponding exponential map is injective for all vectors ``c > 0``.
+To check surjectivity, we need to verify two conditions.
+First, we check the face condition.
+For this purpose, we compute the cocircuits of the oriented matroids
+corresponding to the matrices::
+
     sage: cc1 = cocircuits_from_matrix(W, kernel=False)
     sage: cc1
     [(+0+0), (0+0+), (-0-0), (0-0-)]
     sage: cc2 = cocircuits_from_matrix(Wt, kernel=False)
     sage: cc2
     [(+++0), (-00+), (0+++), (---0), (+00-), (0---)]
+
+Here, we are only interested in the positive cocircuits::
+
     sage: cc1p = [X for X in cc1 if X > 0]
     sage: cc1p
     [(+0+0), (0+0+)]
     sage: cc2p = [X for X in cc2 if X > 0]
     sage: cc2p
     [(+++0), (0+++)]
+
+Since every sign vector in ``cc2p`` has a smaller element in ``cc1p``,
+the face condition is satisfied.
+There is also a function in the package that can be used directly
+to check whether this condition is fulfilled::
+
     sage: cond_faces(W, Wt)
     True
+
+We need to check a third condition to verify surjectivity.
+For this purpose, we consider again the oriented matroid determined by ``W``::
+
     sage: covectors_from_matrix(W, kernel=True)
     [(0000), (-0+0), (0-0+), (+0-0), (0+0-), (-++-), (++--), (+--+), (--++)]
+
+Since there is no positive covector,
+the exponential map is surjective.
+The package offers a function to check this condition condition::
+
     sage: cond_nondegenerate(W, Wt)
     True
+
+Hence, the exponential map is bijective.
+
+Let us consider another example.
+We swap the two matrices from before::
+
     sage: W = matrix([[1,0,0,-1],[0,1,1,1]])
     sage: W
     [ 1  0  0 -1]
@@ -45,22 +95,42 @@ EXAMPLES::
     sage: Wt
     [1 0 1 0]
     [0 1 0 1]
+
+Because of symmetry, the corresponding exponential map is injective::
+
     sage: cond_inj_intersection(W, Wt)
     True
+
+Now, we attempt to check the face condition::
+
     sage: cc1 = cocircuits_from_matrix(W, kernel=False)
     sage: cc1
     [(+++0), (-00+), (0+++), (---0), (+00-), (0---)]
     sage: cc2 = cocircuits_from_matrix(Wt, kernel=False)
     sage: cc2
     [(+0+0), (0+0+), (-0-0), (0-0-)]
+
+Again, we are only interested in the positive cocircuits::
+
     sage: cc1p = [X for X in cc1 if X > 0]
     sage: cc1p
     [(+++0), (0+++)]
     sage: cc2p = [X for X in cc2 if X > 0]
     sage: cc2p
     [(+0+0), (0+0+)]
+
+Therefore, condition does not hold.
+We also apply the corresponding function from the package::
+
     sage: cond_faces(W, Wt)
     False
+
+Consequently, this map is not bijective.
+
+Now, we consider Example 20 from [MHR19]_.
+Here, we have a parameter ``wt > 0``.
+Depending on this parameter, the resulting exponential map will be bijective::
+
     sage: var('wt')
     wt
     sage: W = matrix(3, 6, [0,0,1,1,-1,0,1,-1,0,0,0,-1,0,0,1,-1,0,0])
@@ -73,25 +143,56 @@ EXAMPLES::
     [ 1  1  0  0 -1 wt]
     [ 1 -1  0  0  0  0]
     [ 0  0  1 -1  0  0]
+
+The first two conditions depend on the sign vectors of the corresponding oriented matroids.
+Consequently, the choice of the positive parameter ``wt`` does not affect the result.
+In order to compute the sign vectors, we set ``wt`` to ``1``::
+
     sage: cond_inj_intersection(W, Wt(wt=1))
     True
+
+Hence, the map is injective.
+Also the face condition is satisfied::
+
     sage: cond_faces(W, Wt(wt=1))
     True
+
+For specific values of ``wt``, the pair of subspaces
+determined by kernels of the matrices is non-degenerate.
+This is the case for :math:`wt \in (0, 1) \cup (1, 2)`::
+
     sage: cond_nondegenerate(W, Wt(wt=1/2))
     True
     sage: cond_nondegenerate(W, Wt(wt=3/2))
     True
+
+On the other hand, this condition does not hold if
+:math:`wt \in {1} \cup [2, \infty)`.
+In this case, the exponential map is injective but not surjective::
+
     sage: cond_nondegenerate(W, Wt(wt=1))
     False
     sage: cond_nondegenerate(W, Wt(wt=2))
     False
     sage: cond_nondegenerate(W, Wt(wt=3))
     False
+
+We consider some final example::
+
     sage: from bijectivity_exponential_maps.conditions_bijectivity import nondeg_cond1
     sage: W = matrix([[1,1,0,0],[0,0,1,0]]).right_kernel_matrix()
     sage: Wt = matrix([[1,0,2,0],[0,1,0,-1]])
+
+Now, we check whether the non-degeneracy condition is satisfied::
+
     sage: nondeg_cond1(W, Wt, certificate=True)
     [False, [[[2], [0, 1]], (2, 2, 4, -2)]]
+
+From the output, we see that the condition is violated.
+The vector ``(2, 2, 4, −2)`` lies in the row space
+of ``Wt`` and corresponds to the positive sign vectors
+``(00+0)`` and ``(++00)``
+that are represented by the sets ``{2}`` and ``{0, 1}``.
 """
 
 #############################################################################
