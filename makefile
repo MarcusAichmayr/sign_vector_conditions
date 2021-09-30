d49dbcd05ed5f11e4a1f1be64a7cd5f81e7da738
Marcus <aichmayr.marcus@gmail.com>
2021-09-30 09:54:40 +0200

    use doctests for injectivity

--- makefile
+++ makefile
@@ -5,8 +5,9 @@ install:
 
 test:
 	sage -t bijectivity_exponential_maps/functions.py
-	sage tests/test_conditions_injectivity.py -v
+	sage -t bijectivity_exponential_maps/conditions_injectivity.py
+	sage tests/test_conditions_injectivity.py -v # remove this later
 	sage -t bijectivity_exponential_maps/conditions_bijectivity.py
 	sage tests/test_conditions_bijectivity_robust.py -v
-	# Todo: tests for utility
+	# TODO: tests for utility
 
--- tests/test_conditions_injectivity.py
+++ tests/test_conditions_injectivity.py
@@ -15,26 +15,5 @@ class Tests(unittest.TestCase):
             else:
                 self.assertEqual(cond_inj_intersection(A, B), cond_inj_minors(A, B))
 
-    def test_cond_inj_intersection_empty(self):
-        A = identity_matrix(3)
-        B = A # kernel of B is empty
-        self.assertTrue(cond_inj_intersection(A, B))
-
-    def test_geq(self):
-        l = [0, 5, 1]
-        self.assertTrue(geq(l))
-        l = [0, 0]
-        self.assertTrue(geq(l))
-        l = [0, -5]
-        self.assertFalse(geq(l))
-        var('x')
-        l = [x, x**2 + 1, -1, 5]
-        self.assertFalse(geq(l))
-
-    def test_geq_leq_symbolic(self):
-        var('x')
-        l = [x, x**2 + 1, -1, 5]
-        self.assertFalse(geq_leq(l))
-
 if __name__ == '__main__':
     unittest.main()
