7d98b0db406e78c199c2f4b5b4a24d991ea850ce
MarcusAichmayr <aichmayr.marcus@gmail.com>
2023-03-08 23:15:54 +0100

    rename 'exists_normal_vector'

--- bijectivity_exponential_maps/conditions_bijectivity.py
+++ bijectivity_exponential_maps/conditions_bijectivity.py
@@ -209,7 +209,7 @@ that are represented by the sets ``{2}`` and ``{0, 1}``.
 
 from .utility import normalize, pos_cocircuits_from_matrix, pos_covectors_from_matrix
 from elementary_vectors import elementary_vectors
-from elementary_vectors import setup_intervals, exists_vector, exists_normal_vector, construct_vector
+from elementary_vectors import setup_intervals, exists_vector, exists_orthogonal_vector, construct_vector
 from sign_vectors.oriented_matroids import cocircuits_from_matrix
 
 from sage.modules.free_module_element import zero_vector
@@ -477,7 +477,7 @@ def nondeg_cond1(W, Wt, certificate=False):
                     else:
                         if certificate:
                             for v in evs:
-                                if exists_normal_vector(v, intervals):
+                                if exists_orthogonal_vector(v, intervals):
                                     proof.append([v, I + [X.support()]])
                                     break
 
